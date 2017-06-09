b20594c6507e3a4548cca56768cb05b6fc993cdd
Kai Curry <kai@cape.io>
2017-06-08 20:12:27 -0400

    add dispatch reset action`

--- index.js
+++ index.js
@@ -32,10 +32,12 @@ store.subscribe(() =>
 // The only way to mutate the internal state is to dispatch an action.
 // The actions can be serialized, logged or stored and later replayed.
 store.dispatch({ type: 'INCREMENT' })
-// 1
+// 8
 store.dispatch({ type: 'INCREMENT' })
-// 2
+// 9
 store.dispatch({ type: 'DECREMENT' })
-// 1
+// 8
 store.dispatch({ type: 'SET', payload: 3 })
 // 3
+store.dispatch({ type: 'RESET' })
+// 7
