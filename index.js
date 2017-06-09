845dbdc429b79f808c5ffa7d80e1dea6ece0f893
Kai Curry <kai@cape.io>
2017-06-08 20:09:09 -0400

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+import { createStore } from 'redux'
+
+const initState = 7
+
+function zoomLevel(state = initState, action) {
+  switch (action.type) {
+  case 'INCREMENT':
+    return state + 1
+  case 'DECREMENT':
+    return state - 1
+  case 'SET':
+    return action.payload
+  case 'RESET':
+    return initState
+  default:
+    return state
+  }
+}
+
+// Create a Redux store holding the state of your app.
+// Its API is { subscribe, dispatch, getState }.
+let store = createStore(zoomLevel)
+
+// You can use subscribe() to update the UI in response to state changes.
+// Normally you'd use a view binding library (e.g. React Redux) rather than subscribe() directly.
+// However it can also be handy to persist the current state in the localStorage.
+
+store.subscribe(() =>
+  console.log(store.getState())
+)
+
+// The only way to mutate the internal state is to dispatch an action.
+// The actions can be serialized, logged or stored and later replayed.
+store.dispatch({ type: 'INCREMENT' })
+// 1
+store.dispatch({ type: 'INCREMENT' })
+// 2
+store.dispatch({ type: 'DECREMENT' })
+// 1
+store.dispatch({ type: 'SET', payload: 3 })
+// 3
