0281106c778750ada76fa38159dbec65d8b9758b
Kai Curry <kai@cape.io>
2017-06-13 18:56:53 -0400

    screen size

---
+++ water/screen/actions.js
@@ -0,0 +1,9 @@
+export const IS_MOBILE = 'screen/MOBILE'
+export function setIsMobile() {
+  return { type: IS_MOBILE }
+}
+
+export const NOT_MOBILE = 'screen/NOT_MOBILE'
+export function setNotMobile() {
+  return { type: NOT_MOBILE }
+}
