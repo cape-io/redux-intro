6460b27f51ecf53f439bb2a58552100d597c023b
Kai Curry <kai@cape.io>
2017-06-13 19:27:37 -0400

    screen actions

--- water/screen/actions.js
+++ water/screen/actions.js
@@ -7,3 +7,21 @@ export const NOT_MOBILE = 'screen/NOT_MOBILE'
 export function setNotMobile() {
   return { type: NOT_MOBILE }
 }
+
+export function getMobileAction(isMobile) {
+  return isMobile ? setIsMobile() : setNotMobile()
+}
+
+function getWindowWidth() {
+  return Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+}
+const skinnyWindow = gt(960)
+const checkSkinny = flow(getWindowWidth, skinnyWindow)
+
+export function getWindowAction() {
+  return (dispatch, getState) => {
+    const oldIsMobile = getIsMobile(getState())
+    const newIsMobile = checkSkinny()
+    if (newIsMobile !== oldIsMobile) return getMobileAction(newIsMobile)
+  }
+}
